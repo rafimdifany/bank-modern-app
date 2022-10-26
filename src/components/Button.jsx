cd8ef484ccb8842e344ad30c3ec536d77c37bb5b
rafimdifany <rafimdifany@gmail.com>
2022-10-26 20:26:41 +0700

    add CardDeal Section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Button = ({styles}) => {
+const Button = ({styles, text = "Get Started"}) => {
   return (
-    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
-      Get Started
+    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}>
+      {text}
     </button>
   )
 }
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { card } from "../assets"
+import styles, { layout } from "../style"
+import Button from "./Button"
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={`${styles.heading2}`}>Find a better card deal <br className={`sm:block hidden`}/> in few easy steps.</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio iste fugiat facere? Culpa ad inventore placeat similique, laborum eligendi, nesciunt illo suscipit voluptatibus et perspiciatis.</p>
+
+      <Button styles="mt-10" text="View More"/>
+    </div>
+
+    <div className={`${layout.sectionImg} flex-col`}>
+      <img src={card} alt="card" />
+      </div>
+  </section>
+)
 
 export default CardDeal
